c4d60140d2a6ad914fbac0d1fa8836c559639a7f
gonzazoid <alloaolla@gmail.com>
2024-01-08 12:39:05 +0300

    first draft

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-export publishHashMessage from "./src/publish-hash-message.js";
-export publishSignedMessage from "./src/publish-signed-message.js";
-export postMessage from "./src/post-message.js";
+import publishHashMessage from "./src/publish-hash-message.js";
+import publishSignedMessage from "./src/publish-signed-message.js";
+import postMessage from "./src/post-message.js";
+
+export {
+  publishHashMessage,
+  publishSignedMessage,
+  postMessage
+}
